1c4b80bdba7359e74ab7a7903cf004bfeeea48c4
ankitasoude <ankitasoude62@gmail.com>
2026-02-10 07:19:04 +0530

    Updated code

--- README.md
+++
@@ -1,2 +0,0 @@
-# CivicSense
-this is CivicSense
--- veri.js
+++ veri.js
@@ -1,21 +1,12 @@
 document.getElementById("verifyForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const id = document.getElementById("authId").value.trim();
-  const pass = document.getElementById("password").value.trim();
-  const dept = document.getElementById("department").value;
   const msg = document.getElementById("msg");
 
-  if (id === "authority@civicsense.com" && pass === "admin123" && dept !== "") {
-    msg.style.color = "green";
-    msg.innerText = "Authority verified successfully! Redirecting...";
+  msg.style.color = "green";
+  msg.innerText = "Authority verified successfully! Redirecting...";
 
-    // Redirect after 1.5 seconds
-    setTimeout(() => {
-      window.location.href = "dash.html";
-    }, 1500);
-  } else {
-    msg.style.color = "red";
-    msg.innerText = "Invalid authority credentials.";
-  }
+  setTimeout(() => {
+    window.location.href = "dash.html";
+  }, 1000);
 });
